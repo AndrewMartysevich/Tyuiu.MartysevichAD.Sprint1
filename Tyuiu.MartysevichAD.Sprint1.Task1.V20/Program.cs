1d510506003ed5e01dc5fd2beb18ee0f904cf91c
Tyuiu.MartysevichAD <Tyuiu.MartysevichAD@outlook.com>
2023-10-04 23:55:54 +0500

    Task1

---
+++ Tyuiu.MartysevichAD.Sprint1.Task1.V20/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MartysevichAD.Sprint1.Task1.V20.Lib;
+
+namespace Tyuiu.MartysevichAD.Sprint1.Task1.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Мартысевич А. Д. | ИИПб-23-3";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #1                                                              *");
+            Console.WriteLine("* Тема: Организация ввода/вывода в консольных приложениях                *");
+            Console.WriteLine("* Задание #1                                                             *");
+            Console.WriteLine("* Вариант #20                                                            *");
+            Console.WriteLine("* Выполнил: Мартысевич Андрей Денисович | ИИПб-23-3                      *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,*");
+            Console.WriteLine("* вычисляет результат по формуле (x*y/2) + 10 и печатает его на экране.  *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            double x, y;
+            Console.WriteLine("Введите значение x");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение y");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadLine();
+        }
+    }
+}
